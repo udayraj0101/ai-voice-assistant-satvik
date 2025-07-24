75909e4242398fb480cac348c7fa4195caf88448
udayraj0101 <uday72192@gmail.com>
2025-07-24 18:01:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,31 @@
-import { join, dirname } from "path";
+import { join, dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 
 const path = fileURLToPath(import.meta.url);
 
-export default {
-  root: join(dirname(path), "client"),
-  plugins: [react()],
-  build: {
-    outDir: "../dist/client",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: join(dirname(path), "client/index.html"),
+export default ({ mode }) => {
+  const isSSR = mode === "ssr";
+
+  console.log("Resolved outDir:", isSSR
+    ? resolve(__dirname, "dist/server")
+    : resolve(__dirname, "dist/client")
+  );
+
+  return {
+    root: join(dirname(path), "client"),
+    plugins: [react()],
+    build: {
+      outDir: isSSR
+        ? resolve(__dirname, "dist/server") // Server build output
+        : resolve(__dirname, "dist/client"), // Client build output
+      emptyOutDir: true,
+      rollupOptions: {
+        input: isSSR
+          ? join(dirname(path), "client/entry-server.jsx") // SSR entry point
+          : join(dirname(path), "client/index.html"), // Client entry point
       },
+      ssr: isSSR ? join(dirname(path), "client/entry-server.jsx") : undefined,
     },
-  },
+  };
 };
